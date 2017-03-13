17bcaa571fa7dc2f4d7afc8bd5f44723b9d6bc1a
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-03-13 14:06:20 -0500

    instructions

---
+++ client.js
@@ -0,0 +1,25 @@
+var index = 0;
+
+
+
+$(document).ready(function(){
+  appendDom();
+});
+
+function appendDom() {
+  $('button').on("click", function() {
+    index++;
+    $('.container').append('<div class="newDiv">' +
+                            '<p>'+index+'</p>' +
+                            '<button class="swap">Swap</button>'+
+                            '<button class="delete">Delete</button>'+
+                            '</div>');
+    $('.swap').on("click", function(){
+    $(this).parents('div.newDiv').css('background-color', 'yellow');
+    });
+
+    $('.delete').on("click", function(){
+    $(this).parents('div.newDiv').hide('newDiv');
+    });
+  });
+}
